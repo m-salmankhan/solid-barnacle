5f980a38fcf838ca9bc634af9dd16b65958a55a1
Salman Khan <salman@msalmankhan.co.uk>
2021-07-31 04:29:40 +0100

    logs

--- out/index.js
+++ out/index.js
@@ -4,6 +4,8 @@ try {
     var fileExtensions = core.getInput('file-extensions');
     var style = core.getInput('style');
     var exclude_dirs = core.getInput('exclude-dirs');
+    var token = core.getInput('token');
+    var octokit = github.getOctokit(token);
     console.log("File Extensions: " + fileExtensions);
     console.log("Style: " + style);
     console.log("Exclude: " + exclude_dirs);
@@ -12,6 +14,12 @@ try {
     console.log(github.context);
     console.log("========= issue ==========");
     console.log(github.context.issue);
+    var data = octokit.rest.pulls.get({
+        owner: github.context.issue.owner,
+        repo: github.context.issue.repo,
+        pull_number: github.context.issue.number,
+    });
+    console.log(data);
 }
 catch (error) {
     core.setFailed(error.message);
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ try {
     const fileExtensions = core.getInput('file-extensions');
     const style = core.getInput('style');
     const exclude_dirs = core.getInput('exclude-dirs');
+    const token = core.getInput('token');
+
+    const octokit = github.getOctokit(token)
+
     console.log(`File Extensions: ${fileExtensions}`);
     console.log(`Style: ${style}`);
     console.log(`Exclude: ${exclude_dirs}`);
@@ -15,6 +19,14 @@ try {
 
     console.log("========= issue ==========")
     console.log(github.context.issue)
+
+    const data = octokit.rest.pulls.get({
+        owner: github.context.issue.owner,
+        repo: github.context.issue.repo,
+        pull_number: github.context.issue.number,
+    });
+
+    console.log(data)
 } catch (error) {
     core.setFailed(error.message);
 }
