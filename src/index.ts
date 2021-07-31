76306c5c571c6fb0a60711d25ab279ad14e9e520
Salman Khan <salman@msalmankhan.co.uk>
2021-07-31 04:39:31 +0100

    logs

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    const fileExtensions = core.getInput('file-extensions');
-    const style = core.getInput('style');
-    const exclude_dirs = core.getInput('exclude-dirs');
-    const token = core.getInput('token');
+async function run() {
+    try {
+        const fileExtensions = core.getInput('file-extensions');
+        const style = core.getInput('style');
+        const exclude_dirs = core.getInput('exclude-dirs');
+        const token = core.getInput('token');
 
-    const octokit = github.getOctokit(token)
+        const octokit = github.getOctokit(token)
 
-    console.log(`File Extensions: ${fileExtensions}`);
-    console.log(`Style: ${style}`);
-    console.log(`Exclude: ${exclude_dirs}`);
-    console.log(`token: ${style}`);
+        console.log(`File Extensions: ${fileExtensions}`);
+        console.log(`Style: ${style}`);
+        console.log(`Exclude: ${exclude_dirs}`);
+        console.log(`token: ${style}`);
 
-    console.log("========= CONTEXT ==========")
-    console.log(github.context)
+        console.log("========= CONTEXT ==========")
+        console.log(github.context)
 
-    console.log("========= issue ==========")
-    console.log(github.context.issue)
+        console.log("========= issue ==========")
+        console.log(github.context.issue)
 
-    const data = octokit.rest.pulls.get({
-        owner: github.context.issue.owner,
-        repo: github.context.issue.repo,
-        pull_number: github.context.issue.number,
-    });
+        const data = await octokit.rest.pulls.get({
+            owner: github.context.issue.owner,
+            repo: github.context.issue.repo,
+            pull_number: github.context.issue.number,
+        });
 
-    console.log(data)
-} catch (error) {
-    core.setFailed(error.message);
+        console.log(data)
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
