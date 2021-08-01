1a08534aea5b69fac9229b6893c11ba5bb63dc06
Salman Khan <salman@msalmankhan.co.uk>
2021-08-01 19:46:28 +0100

    reinstaled npm modules

---
+++ build/src/constants.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.requiredBinaries = exports.pythonExtensions = exports.clangExtensions = void 0;
+exports.clangExtensions = ["c", "h", "cpp", "java", "json", "js"];
+exports.pythonExtensions = ["py", "py"];
+exports.requiredBinaries = ["python", "pip", "node", "clang-format"];
+//# sourceMappingURL=constants.js.map
